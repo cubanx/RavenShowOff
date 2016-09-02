5575d68197193cc3f7fc5fa0ab2b646ec91b1874
cubanx <github@rickdiaz.com>
2016-09-02 14:15:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DocoptNet;
+
+namespace RavenShowOff
+{
+    internal class Program
+    {
+        private const string usage = @"Naval Fate.
+
+    Usage:
+      RavenShowOff.exe save <id>
+      RavenShowOff.exe load <id>
+      RavenShowOff.exe upgrade <app_version>
+
+    Options:
+      -h --help     Show this screen.
+    ";
+
+        private static void Main(string[] args)
+        {
+            var arguments = new Docopt().Apply(usage, args);
+            foreach (var argument in arguments)
+                Console.WriteLine($"{argument.Key} = {argument.Value}");
+        }
+    }
+}
